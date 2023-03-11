bd13c1e338e85913623a35c9b7e6678f8a8fa519
syed faizan <98455313+syed92809@users.noreply.github.com>
2023-03-11 20:54:49 +0500

    Add files via upload

---
+++ instaScraper.py
@@ -0,0 +1,20 @@
+import re
+
+import instaloader
+
+instaScrap=instaloader.Instaloader()
+profile=instaloader.Profile.from_username(instaScrap.context, 'syedfaizan844')
+print("User Name:", profile.username)
+print("User Id:",profile.userid)
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", profile.biography)
+print("Email:",emails)
+print("Number of post:",profile.mediacount)
+print("Following:",profile.followees)
+print("Followers:",profile.followers)
+print("URL:",profile.external_url)
+
+# instaScrap.login(user="ahmed03362021",passwd="ahmed..12")
+# profile=instaloader.Profile.from_username(instaScrap.context, 'wealth')
+# posts=profile.get_posts()
+# for index, post in enumerate(posts,1):
+#     instaScrap.download_post(post, target=f"{profile.username}_{index}")
